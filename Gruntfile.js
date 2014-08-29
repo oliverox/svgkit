cf46c7a5bbb225f5a89c77cd778692dfbfaf4c08
Oliver Oxenham <ooxenham@stubhub.com>
2014-08-29 11:28:49 -0700

    updated svgs

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,7 +46,7 @@ module.exports = function(grunt) {
     // test task to run unit tests
     grunt.registerTask('createdemo', '', function () {
         var svgContent = grunt.file.read('dest/sprite.svg');
-        grunt.file.write('dest/injectsprite.js', 'document.addEventListener(\'DOMContentLoaded\', function() { var svgSprite = document.querySelectorAll(\'.svg-sprite\'); svgSprite[0].innerHTML = \'' + svgContent + '\'; });');
+        grunt.file.write('dest/injectsprite.js', 'var svgSprite = document.querySelectorAll(\'.svg-sprite\'); svgSprite[0].innerHTML = \'' + svgContent + '\';');
         var styles = "svg { width:30px; height:30px; } .svg-sprite { display:none; } svg:hover { fill:orange; }";
         var spritedemohtml = "<!doctype html><html><head><title>SVG-Sprite</title><style>" + styles + "</style><head><body><div class='svg-sprite'></div><script type='text/javascript' src='injectsprite.js'></script>";
         var icons = svgContent.split('<title>');
@@ -54,7 +54,10 @@ module.exports = function(grunt) {
             console.log(icons[i].split('</title>')[0]);
             spritedemohtml += "<svg class='icon-" + icons[i].split('</title>')[0] + "'><use class='path1' xlink:href='#icon-" + icons[i].split('</title>')[0] + "'></use></svg>";
         }
-        spritedemohtml += "</body></html>";
+        spritedemohtml += "</body><script>";
+        spritedemohtml += "var parentEl = document.getElementsByTagName('body')[0];parentEl.addEventListener('touchend', function(evt) {";
+        spritedemohtml += "if (evt.target.correspondingUseElement) { console.log(evt.target.correspondingUseElement.parentNode.className.baseVal);} else { console.log(evt.target.className.baseVal);}});</script>";
+        spritedemohtml += "</html>";
         grunt.file.write('dest/spritedemo.html', spritedemohtml);
     });
     grunt.registerTask('default', 'Builds sprite file icons.svg', ['svgstore', 'svgmin', 'createdemo']);
